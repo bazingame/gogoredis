3fc447bb57180bb20b2adf448d2836d385ea9776
fenghuayu <fenghuayu@zhihu.com>
2021-04-16 03:17:54 +0800

    add some func for linked list

--- pkg/datastruct/linkedlist.go
+++ pkg/datastruct/linkedlist.go
@@ -1,1 +1,196 @@
 package datastruct
+
+//list *listCreate(void)
+//void listRelease(list *list)
+//list *listAddNodeHead(list *list, void *value)
+//list *listAddNodeTail(list *list, void *value)
+//list *listInsertNode(list *list, listNode *old_node, void *value, int after)
+//void listDelNode(list *list, listNode *node)
+//listIter *listGetIterator(list *list, int direction)
+//listNode *listNext(listIter *iter)
+//void listReleaseIterator(listIter *iter)
+//list *listDup(list *orig)
+//listNode *listSearchKey(list *list, void *key)
+//listNode *listIndex(list *list, long index)
+//void listRewind(list *list, listIter *li)
+//void listRewindTail(list *list, listIter *li)
+//void listRotate(list *list)
+
+type ListNode struct {
+	prev  *ListNode
+	next  *ListNode
+	value *interface{}
+}
+
+type List struct {
+	head  *ListNode
+	tail  *ListNode
+	len   int
+	dup   func()
+	free  func()
+	match func()
+}
+
+func (l *List) ListLength() int {
+	return l.len
+}
+
+func (l *List) ListFirst() *ListNode {
+	return l.head
+}
+func (l *List) ListLast() *ListNode {
+	return l.tail
+}
+
+func (l *ListNode) ListPrevNode() *ListNode {
+	return l.prev
+}
+
+func (l *ListNode) ListNextNode() *ListNode {
+	return l.next
+}
+
+func (l *ListNode) ListNodeValue() *interface{} {
+	return l.value
+}
+
+func (l *List) ListSetDupMethod(dupF func()) *List {
+	l.dup = dupF
+	return l
+}
+
+func (l *List) ListSetFreeMethod(freeF func()) *List {
+	l.dup = freeF
+	return l
+}
+
+func (l *List) ListSetMatchMethod(matchF func()) *List {
+	l.dup = matchF
+	return l
+}
+
+func ListCreate() *List {
+	list := new(List)
+	list.len = 0
+	list.head = nil
+	list.tail = nil
+	return list
+}
+
+func (l *List) ListRelease() *List {
+	panic("implement me")
+	return nil
+}
+
+func (l *List) ListAddNodeHead(v *interface{}) *List {
+	node := new(ListNode)
+	node.value = v
+	if l.len == 0 {
+		l.head = node
+		l.tail = node
+	} else {
+		node.next = l.head
+		l.head.prev = node
+		l.head = node
+	}
+	l.len++
+	return l
+}
+
+func (l *List) ListAddNodeTail(v *interface{}) *List {
+	node := new(ListNode)
+	node.value = v
+	if l.len == 0 {
+		l.head = node
+		l.tail = node
+	} else {
+		node.prev = l.tail
+		l.tail.next = node
+		l.tail = node
+	}
+	l.len++
+	return l
+}
+
+func (l *List) ListInsertNode(oldNode *ListNode, v *interface{}, after bool) *List {
+	node := new(ListNode)
+	node.value = v
+
+	if after {
+		if oldNode == l.tail { // oldNode.next != nil
+			l.tail = node
+		}
+		node.next = oldNode.next
+		node.prev = oldNode
+	} else {
+		if oldNode == l.head { //oldNode.prev != nil
+			l.head = node
+		}
+		node.prev = oldNode.prev
+		node.next = oldNode
+	}
+
+	if node.prev != nil {
+		node.prev.next = node
+	}
+	if node.next != nil {
+		node.next.prev = node
+	}
+
+	l.len++
+	return nil
+}
+
+func (l *List) ListDelNode(oldNode *ListNode) *List {
+	if l.head == oldNode {
+		l.head = oldNode.next
+	} else {
+		oldNode.prev.next = oldNode.next
+	}
+
+	if l.tail == oldNode {
+		l.tail = oldNode.prev
+	} else {
+		oldNode.next.prev = oldNode.prev
+	}
+	oldNode.prev = nil
+	oldNode.next = nil
+	l.len--
+	return l
+}
+
+func (l *List) ListGetIterator() *List {
+	return nil
+}
+
+func (l *List) ListNext() *List {
+	return nil
+}
+
+func (l *List) ListReleaseIterator() *List {
+	return nil
+}
+
+func (l *List) ListDup() *List {
+	return nil
+}
+
+func (l *List) ListSearchKey() *List {
+	return nil
+}
+
+func (l *List) ListIndex() *List {
+	return nil
+}
+
+func (l *List) ListRewind() *List {
+	return nil
+}
+
+func (l *List) ListRewindTail() *List {
+	return nil
+}
+
+func (l *List) ListRotate() *List {
+	return nil
+}
--- pkg/datastruct/sds.go
+++ pkg/datastruct/sds.go
@@ -1,14 +1,124 @@
 package datastruct
 
-type SDS interface {
-	SdsNew()
-	SdsEmpty()
-	SdsFree()
-	SdsLen()
-	SdsClear()
-	SdsCat()
-	SdsCpy()
-	SdsRange()
-	SdsTrim()
-	SdsCmp()
+const (
+	SDSMaxPrealloc = 1024 * 1024 // 最大预分配长度 1M
+)
+
+type SDS struct {
+	free int
+	len  int
+	buf  []byte
+}
+
+// SDSNew Create a new sds string starting from a null terminated string
+func SDSNew(initStr string) *SDS {
+	var strLen int
+	strLen = len(initStr)
+	return sdsNewLen(&initStr, strLen)
+}
+
+// SDSEmpty Create an empty (zero length) sds string
+func SDSEmpty() *SDS {
+	return sdsNewLen(nil, 0)
+}
+
+func (s *SDS) Len() int {
+	return s.len
+}
+func (s *SDS) Avail() int {
+	return s.free
+}
+func (s *SDS) Buf() []byte {
+	return s.buf[:s.len]
+}
+
+// Clear  Modify an sds string on-place to make it empty (
+func (s *SDS) Clear() *SDS {
+	s.free += s.len
+	s.len = 0
+	return s
+}
+
+// Cat Append the specified null terminated string to the sds string 's'
+func (s *SDS) Cat(str string) *SDS {
+	return s.sdsCatLen(&str, len(str))
+}
+
+//func (s *SDS) Free() *SDS {
+//	s.free += s.len
+//	s.len = 0
+//	return s
+//}
+
+func (s *SDS) Cpy(str string) *SDS {
+	return s.sdsCpyLen(&str, len(str))
+}
+
+func (s *SDS) Range(start, end int) *SDS {
+	s.buf = s.buf[start:end]
+	s.len = end - start + 1
+	return s
+}
+
+// Trim Remove the part of the string from left and from right
+func (s *SDS) Trim() *SDS {
+	panic("implement me")
+	return s
+}
+
+// Cmp Compare two sds strings s1 and s2
+func (s *SDS) Cmp() *SDS {
+	panic("implement me")
+	return s
+}
+
+// SDSNewLen  Create a new sds string with the content specified by the 'initStr' pointer and 'initLen'.
+func sdsNewLen(initStr *string, initLen int) *SDS {
+	var sh = new(SDS) // todo optimize memory apply
+	sh.len = initLen
+	sh.free = 0
+	if initLen != 0 && initStr != nil {
+		sh.buf = []byte(*initStr)
+	}
+	return sh
+}
+
+func (s *SDS) sdsCatLen(str *string, len int) *SDS {
+	// 计算是否申请新内存, 小于的话直接追加
+
+	s.sdsMakeRoomFor(len)
+
+	copy(s.buf[s.len:], *str)
+	s.len = s.len + len
+
+	s.free = s.free - len
+	return s
+}
+
+func (s *SDS) sdsCpyLen(str *string, len int) *SDS {
+	s.sdsMakeRoomFor(len - s.len)
+
+	copy(s.buf, *str)
+	s.free = s.len + s.free - len
+	s.len = len
+
+	return s
+}
+
+func (s *SDS) sdsMakeRoomFor(addLen int) {
+	if addLen <= s.free {
+		return
+	}
+
+	var newLen = s.len + addLen
+	if newLen < SDSMaxPrealloc {
+		newLen *= 2
+	} else {
+		newLen += SDSMaxPrealloc
+	}
+	oldBuf := s.buf
+	s.buf = make([]byte, newLen)
+	copy(s.buf, oldBuf)
+	s.free = newLen - s.len
+	return
 }
